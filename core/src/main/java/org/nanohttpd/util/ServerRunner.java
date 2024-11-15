498b552e483628847c47d6cba2d6232a497cf721
alignedfibers <starsnic@gmail.com>
2024-11-15 03:51:38 -0600

    Complained about source compatibility. changed it to Java 11 seems to have corected

--- core/src/main/java/org/nanohttpd/util/ServerRunner.java
+++ core/src/main/java/org/nanohttpd/util/ServerRunner.java
@@ -67,7 +67,7 @@ public class ServerRunner {
 
     public static <T extends NanoHTTPD> void run(Class<T> serverClass) {
         try {
-            executeInstance(serverClass.getDeclaredConstructor().newInstance());
+            executeInstance(serverClass.newInstance());
         } catch (Exception e) {
             ServerRunner.LOG.log(Level.SEVERE, "Could not create server", e);
         }
